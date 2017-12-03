83e502b6d58bd7148f6a2017689b3575bf09f44a
nepdave <dave@perka.com>
2017-12-03 14:08:31 -0500

    got the correct data structure for unmarshaling

--- endpoints.go
+++ endpoints.go
@@ -1,4 +1,4 @@
-package main
+package oanda
 
 import (
 	"fmt"
@@ -7,7 +7,6 @@ import (
 	"net/url"
 	"os"
 )
-
 
 var oandaUrl string = "https://api-fxpractice.oanda.com/v3"
 var bearer string = "Bearer " + os.Getenv("OANDA_TOKEN")
@@ -20,32 +19,33 @@ prices
 */
 
 func GetPricing(instrument string) []byte {
-        client := &http.Client{}
-        queryValues := url.Values{}
-        queryValues.Add("instruments", instrument)
+	client := &http.Client{}
+	queryValues := url.Values{}
+	queryValues.Add("instruments", instrument)
 
-        req, err := http.NewRequest("GET", oandaUrl+"/accounts/"+accountId+
-																		"/pricing?"+queryValues.Encode(), nil)
+	req, err := http.NewRequest("GET", oandaUrl+"/accounts/"+accountId+
+		"/pricing?"+queryValues.Encode(), nil)
 
-				req.Header.Add("Content-Type", "application/json")
-				req.Header.Add("Authorization", bearer)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", bearer)
+
+	fmt.Println("GET PRICING!!!")
+	fmt.Println(req.Header)
+	fmt.Println(queryValues)
 
 
-				fmt.Println("GET PRICING!!!")
-				fmt.Println(req.Header)
-				fmt.Println(queryValues)
+	resp, err := client.Do(req)
 
-				resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-        if err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-        }
-
-        defer resp.Body.Close()
-        pricesByte, _ := ioutil.ReadAll(resp.Body)
-        bodyString := string(pricesByte)
-				fmt.Println("ENDPOINTS LINE 47")
-				fmt.Println(bodyString)
-        return pricesByte
+	defer resp.Body.Close()
+	pricesByte, _ := ioutil.ReadAll(resp.Body)
+	bodyString := string(pricesByte)
+	fmt.Println("ENDPOINTS LINE 47")
+	fmt.Println(bodyString)
+	fmt.Println(resp.StatusCode)
+	return pricesByte
 }
--- unmarshal.go
+++ unmarshal.go
@@ -1,40 +1,43 @@
-package main
+package oanda
 
 import (
 	"encoding/json"
 	"time"
 )
 
+type Pricing struct {
+	Prices []Prices  `json: "prices"`
+	Time   time.Time `json: "time"`
+}
 
 type Prices struct {
-	Type        string    `json:"type"`
-	Asks        []Asks    `json:"asks"`
-	Bids        []Bids    `json:"bids"`
-	CloseOutAsk string    `json:"closeoutAsk"`
-	CloseOutBid string    `json:"closeoutBid"`
-	Instrument  string    `json:"instrument"`
-	Status      string    `json:"tradeable"`
-	Time        time.Time `json:"time"`
+	Type        string    `json: "type"`
+	Asks        []Ask     `json: "asks"`
+	Bids        []Bid     `json: "bids"`
+	CloseOutAsk string    `json: "closeoutAsk"`
+	CloseOutBid string    `json: "closeoutBid"`
+	Instrument  string    `json: "instrument"`
+	Status      string    `json: "status"`
+	Time        time.Time `json: "time"`
 }
 
-type Asks struct {
-	Price     float64 `json:"price"`
-	Liquidity string  `json:"liquidity"`
+type Ask struct {
+	Price     string `json: "price"`
+	Liquidity int64  `json: "liquidity"`
+}
+
+type Bid struct {
+	Price     string `json: "price"`
+	Liquidity int64  `json: "liquidity"`
+}
+
+func (p Pricing) UnmarshalPricing(priceByte []byte) *Pricing {
+
+	err := json.Unmarshal(priceByte, &p)
+
+	if err != nil {
+		panic(err)
 	}
 
-type Bids struct {
-	Price     float64 `json:"price"`
-	Liquidity string  `json:"liquidity"`
-}
-
-
-func (p Prices) UnmarshalPricing(priceByte []byte) *Prices {
-
-        err := json.Unmarshal(priceByte, &p)
-
-				if err != nil {
-                panic(err)
-        }
-
-        return &p
+	return &p
 }
