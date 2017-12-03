25d1fac8700c8db4f74e1327d350c53ba89397ca
nepdave <dave@perka.com>
2017-12-03 17:00:11 -0500

    added method to get candle history

--- endpoints.go
+++ endpoints.go
@@ -29,12 +29,7 @@ func GetPricing(instrument string) []byte {
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", bearer)
 
-	fmt.Println("GET PRICING!!!")
-	fmt.Println(req.Header)
-	fmt.Println(queryValues)
-
-
-	resp, err := client.Do(req)
+  resp, err := client.Do(req)
 
 	if err != nil {
 		fmt.Println(err)
@@ -44,8 +39,46 @@ func GetPricing(instrument string) []byte {
 	defer resp.Body.Close()
 	pricesByte, _ := ioutil.ReadAll(resp.Body)
 	bodyString := string(pricesByte)
-	fmt.Println("ENDPOINTS LINE 47")
+	fmt.Println(queryValues)
 	fmt.Println(bodyString)
 	fmt.Println(resp.StatusCode)
+	return pricesByte
+}
+
+  //https://api-fxpractice.oanda.com/v3/instruments/candles?count=10&granularity=D&instruments=EUR_USD
+  //https://api-fxpractice.oanda.com/v3/instruments/instrument/candles?count=10&granularity=D&instruments=EUR_USD
+ //https://api-fxpractice.oanda.com/v3/instrumentsEUR_USD/candles?count=10&granularity=D&instruments=EUR_USD
+//"https://api-fxpractice.oanda.com/v3/instruments/USD_JPY/candles?
+//count=10&price=A&from=2016-01-01T00%3A00%3A00.000000000Z&granularity=D"
+
+//"https://api-fxpractice.oanda.com/v3"
+func GetCandles(instrument string, count string, granularity string) []byte {
+	client := &http.Client{}
+	queryValues := url.Values{}
+	queryValues.Add("instruments", instrument)
+	queryValues.Add("count", count)
+	queryValues.Add("granularity", granularity)
+
+	req, err := http.NewRequest("GET", oandaUrl+"/instruments"+"/"+instrument+
+		"/candles?"+queryValues.Encode(), nil)
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", bearer)
+
+  resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer resp.Body.Close()
+	pricesByte, _ := ioutil.ReadAll(resp.Body)
+	bodyString := string(pricesByte)
+	fmt.Println(queryValues)
+	fmt.Println(bodyString)
+	fmt.Println(resp.StatusCode)
+	fmt.Println(queryValues)
+	fmt.Println(req)
 	return pricesByte
 }
