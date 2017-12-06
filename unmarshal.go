e4c277f47e1ce837ae3c2a49c1347a9eb41d0840
nepdave <dave@perka.com>
2017-12-06 13:08:54 -0500

    removed the space between the colon and the value in the Ohlc struct, its now getting unmarshaled which is weird

--- unmarshal.go
+++ unmarshal.go
@@ -12,29 +12,29 @@ prices
 */
 
 type Pricing struct {
-	Prices []Prices  `json: "prices"`
-	Time   time.Time `json: "time"`
+	Prices []Prices  `json:"prices"`
+	Time   time.Time `json:"time"`
 }
 
 type Prices struct {
-	Type        string    `json: "type"`
-	Asks        []Ask     `json: "asks"`
-	Bids        []Bid     `json: "bids"`
-	CloseOutAsk string    `json: "closeoutAsk"`
-	CloseOutBid string    `json: "closeoutBid"`
-	Instrument  string    `json: "instrument"`
-	Status      string    `json: "status"`
-	Time        time.Time `json: "time"`
+	Type        string    `json:"type"`
+	Asks        []Ask     `json:"asks"`
+	Bids        []Bid     `json:"bids"`
+	CloseOutAsk string    `json:"closeoutAsk"`
+	CloseOutBid string    `json:"closeoutBid"`
+	Instrument  string    `json:"instrument"`
+	Status      string    `json:"status"`
+	Time        time.Time `json:"time"`
 }
 
 type Ask struct {
-	Price     string `json: "price"`
-	Liquidity int64  `json: "liquidity"`
+	Price     string `json:"price"`
+	Liquidity int64  `json:"liquidity"`
 }
 
 type Bid struct {
-	Price     string `json: "price"`
-	Liquidity int64  `json: "liquidity"`
+	Price     string `json:"price"`
+	Liquidity int64  `json:"liquidity"`
 }
 
 func (p Pricing) UnmarshalPricing(priceByte []byte) *Pricing {
@@ -48,18 +48,24 @@ func (p Pricing) UnmarshalPricing(priceByte []byte) *Pricing {
 	return &p
 }
 
-
 type Candles struct {
-	Instrument  string   `json: "instrument"`
-	Granularity string   `json: "granularity"`
-	Candles     []Candle `json: "candles"`
+	Instrument  string   `json:"instrument"`
+	Granularity string   `json:"granularity"`
+	Candles     []Candle `json:"candles"`
 }
 
 type Candle struct {
-	Complete bool                   `json: "complete"`
-	Volume   int64                  `json: "volume"`
-	Time     time.Time              `json: "time"`
-	Mid      map[string]interface{} `json: "mid"`
+	Complete bool      `json:"complete"`
+	Volume   int64     `json:"volume"`
+	Time     time.Time `json:"time"`
+	Mid      Mid       `json:"mid"`
+}
+
+type Mid struct {
+	Open  string `json:"o"`
+	High  string `json:"h"`
+	Low   string `json:"l"`
+	Close string `json:"c"`
 }
 
 func (c Candles) UnmarshalCandles(priceByte []byte) *Candles {
