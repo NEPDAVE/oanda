a96b5ca69a597a69e641384d805461b9cc44554b
NEPDAVE <nepdave1@gmail.com>
2017-12-16 16:52:24 -0800

    added some extra error handling so each endpoint returns a []byte and error

--- endpoints.go
+++ endpoints.go
@@ -1,6 +1,7 @@
 package oanda
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -13,23 +14,23 @@ var oandaUrl string = "https://api-fxpractice.oanda.com/v3"
 var bearer string = "Bearer " + os.Getenv("OANDA_TOKEN")
 var accountId string = os.Getenv("OANDA_ACCOUNT_ID")
 
+//FIXME starting to see a lot of repeat code, might want to write a "communications check"
+//callback function that prints out and logs the good stuff, IE request, Response
+//error codes, maybe even the time
+func CommCheck() {
+	fmt.Println("Communications Check")
+}
+
 /*
 ***************************
 prices
 ***************************
 */
 
-func GetPricing(instruments ...string) []byte {
+func GetPricing(instruments ...string) ([]byte, error) {
 	client := &http.Client{}
 	queryValues := url.Values{}
-
-	//FIXME this is adding instruments=EUR_USD&instruments
-	//=USD_JPY&instruments=GBP_USD&instruments=USD... not good
-	//for _, v := range instruments {
-	//	queryValues.Add("instruments", v)
-	//}
 	instrumentsEncoded := strings.Join(instruments, ",")
-	//instrumentsEncoded := strings.Join(instruments, "%")
 	queryValues.Add("instruments", instrumentsEncoded)
 
 	req, err := http.NewRequest("GET", oandaUrl+"/accounts/"+accountId+
@@ -38,22 +39,30 @@ func GetPricing(instruments ...string) []byte {
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", bearer)
 
-	resp, err := client.Do(req)
-
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return []byte{}, errors.New("GetPricing Error")
 	}
 
-	defer resp.Body.Close()
-	pricesByte, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(pricesByte))
-	fmt.Println(req)
-	fmt.Println(resp.StatusCode)
-	return pricesByte
+	if resp, err := client.Do(req); err != nil {
+		defer resp.Body.Close()
+		pricesByte, _ := ioutil.ReadAll(resp.Body)
+		fmt.Printf("Request: %s\n", req)
+		fmt.Printf("Response: %s\n", string(pricesByte))
+		fmt.Printf("Status Code: %s\n", resp.StatusCode)
+		fmt.Printf("GetPricing Response Error: %s\n", err)
+		return []byte{}, errors.New("GetPricing Error")
+	} else {
+		defer resp.Body.Close()
+		pricesByte, _ := ioutil.ReadAll(resp.Body)
+		fmt.Printf("Request: %s\n", req)
+		fmt.Printf("Response: %s\n", string(pricesByte))
+		fmt.Printf("Status Code: %s\n", resp.StatusCode)
+		fmt.Printf("GetPricing Response Error: %s\n", err)
+		return pricesByte, nil
+	}
 }
 
-func GetCandles(instrument string, count string, granularity string) []byte {
+func GetCandles(instrument string, count string, granularity string) ([]byte, error) {
 	client := &http.Client{}
 	queryValues := url.Values{}
 	queryValues.Add("instruments", instrument)
@@ -66,17 +75,25 @@ func GetCandles(instrument string, count string, granularity string) []byte {
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", bearer)
 
-	resp, err := client.Do(req)
-
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return []byte{}, errors.New("GetPricing Error")
 	}
 
-	defer resp.Body.Close()
-	pricesByte, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(pricesByte))
-	fmt.Println(req)
-	fmt.Println(resp.StatusCode)
-	return pricesByte
+	if resp, err := client.Do(req); err != nil {
+		defer resp.Body.Close()
+		pricesByte, _ := ioutil.ReadAll(resp.Body)
+		fmt.Printf("Request: %s\n", req)
+		fmt.Printf("Response: %s\n", string(pricesByte))
+		fmt.Printf("Status Code: %s\n", resp.StatusCode)
+		fmt.Printf("GetPricing Response Error: %s\n", err)
+		return []byte{}, errors.New("GetCandles Error")
+	} else {
+		defer resp.Body.Close()
+		pricesByte, _ := ioutil.ReadAll(resp.Body)
+		fmt.Printf("Request: %s\n", req)
+		fmt.Printf("Response: %s\n", string(pricesByte))
+		fmt.Printf("Status Code: %s\n", resp.StatusCode)
+		fmt.Printf("GetCandles Response Error: %s\n", err)
+		return pricesByte, nil
+	}
 }
