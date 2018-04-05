d9756b25cd8c2e00c8e5bd6c2a60d4f18e08cee1
nepdave <dave@perka.com>
2018-04-05 14:19:44 -0700

    added testgetcandles function

--- endpoints_test.go
+++ endpoints_test.go
@@ -19,3 +19,16 @@ func TestGetPricing(t *testing.T) {
 	}
 	t.Log("\t\tShould return nil error", checkMark)
 }
+
+//TestGetCandles validates GetPricing returns pricesByte and nil error
+func TestGetCandles(t *testing.T) {
+	_, err := GetPricing("EUR_USD", "50", "D")
+
+	t.Log("Given the need to test Oanda API endpoints")
+	t.Log("\tWhen checking GetCandles function")
+
+	if err != nil {
+		t.Fatal("\t\tShould return nil error", ballotX, err)
+	}
+	t.Log("\t\tShould return nil error", checkMark)
+}
