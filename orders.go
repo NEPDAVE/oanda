9f45cd9b3d739c8a3aa775db77135eb093f30520
nepdave <dave@perka.com>
2018-07-17 08:16:04 -0700

    removed old comments, created a BuyMarketOrder func

--- orders.go
+++ orders.go
@@ -7,30 +7,21 @@ import (
 //"fmt"
 )
 
-//CreateOrder will build the Order sturct needed to submit an order. This maybe
-//FIXME should be a method attached to the Order struct...?
-//FIXME think about creating a CreateBuyOrder and CreateSellOrder func. This will
-//make things more readable. Also think about error handling and possibly another
-//function that will prepare the order and another function that will execute it
-//also take a look at the data structure and make sure it's getting marshalled
-//correctly also add a func to Unmarshal the data after placing an order
-//total side note also lookinto coding the double bb
-func CreateOrder(bid float64, ask float64, instrument string, units int) Orders {
-	//FIXME stopLossPrice and takeProfitPrice are hardcoded to certain ratios
-	//this may not be the best way... infact it may be good to determine these
-	//rations in the fxtechnical package instead.. yes thats a good idea
-	targetPrice := bid
-	stopLossPrice := bid - .00002
+//MarketBuyOrder builds struct needed for marshaling data into a []byte
+func MarketBuyOrder(bid float64, ask float64, instrument string, units int) Orders {
+	stopLossPrice := bid -
 	takeProfitPrice := bid + (ask - bid) - .000002
 	stopLoss := StopLossOnFill{TimeInForce: "GTC", Price: stopLossPrice}
 	takeProfit := TakeProfitOnFill{TimeInForce: "GTC", Price: takeProfitPrice}
+	timeInForce :=
+	type :=
+	positionFill :=
 	orderData := Order{
-		Price:            targetPrice,
 		StopLossOnFill:   stopLoss,
 		TakeProfitOnFill: takeProfit,
 		TimeInForce:      "FOK",
 		Instrument:       instrument,
-		Type:             "LIMIT",
+		Type:             "MARKET",
 		PositionFill:     "DEFAULT"}
 	order := Orders{Order: orderData}
 
