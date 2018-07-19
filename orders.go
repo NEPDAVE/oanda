3900197f3dc48cfad6ac2fb85786a47fcd25e140
nepdave <dave@perka.com>
2018-07-19 09:46:24 -0700

    added fixme to make sl/tp ratios 3/1

--- orders.go
+++ orders.go
@@ -11,7 +11,7 @@ import (
 //https://play.golang.org/p/AC_eqpsQApa
 
 //MarketBuyOrder builds struct needed for marshaling data into a []byte
-//FIXME stopLoss and takeProfit are hard coded?
+//FIXME stopLoss and takeProfit are hard coded? make this ratio 3 to 1
 func MarketBuyOrder(bid float64, ask float64, instrument string, units int) Orders {
 	stopLossPrice := fmt.Sprintf("%.6f", bid-(ask*.005))
 	takeProfitPrice := fmt.Sprintf("%.6f", ask+(ask*.01))
@@ -30,7 +30,7 @@ func MarketBuyOrder(bid float64, ask float64, instrument string, units int) Orde
 }
 
 //MarketSellOrder builds struct needed for marshaling data into a []byte
-//FIXME stopLoss and takeProfit are hard coded?
+//FIXME stopLoss and takeProfit are hard coded? make this ratio 3 to 1
 func MarketSellOrder(bid float64, ask float64, instrument string, units int) Orders {
 	stopLossPrice := fmt.Sprintf("%.6f", ask+(bid*.005))
 	takeProfitPrice := fmt.Sprintf("%.6f", bid-(ask*.01))
