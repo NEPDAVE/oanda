8abdda51f2526b3eff1eb7a62ca303009e703596
nepdave <nepdave1@gmail.com>
2018-07-20 13:22:57 -0700

    created new marshal file with standalong marshaling funcs

--- marshal.go
+++ marshal.go
@@ -2,91 +2,16 @@ package oanda
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 )
 
-/*
-***************************
-orders
-***************************
-*/
-
-//Orders represents entire order(s) object when submiting and order to Oanda
-type Orders struct {
-	OrderData Order `json:"order"`
-}
-
-//Order represents single order to Oanda
-type Order struct {
-	Price            string           `json:"prices"`
-	StopLossOnFill   StopLossOnFill   `json:"stopLossOnFill"`
-	TakeProfitOnFill TakeProfitOnFill `json:"takeProfitOnFill"`
-	TimeInForce      string           `json:"timeInForce"`
-	Instrument       string           `json:"instrument"`
-	Units            int              `json:"units"`
-	Type             string           `json:"type"`
-	PositionFill     string           `json:"positionFill"`
-}
-
-//StopLossOnFill represents stop loss parameters for an Order
-type StopLossOnFill struct {
-	TimeInForce string `json:"timeInForce"`
-	Price       string `json:"price"`
-}
-
-//TakeProfitOnFill represents take profit parameters for an Order
-type TakeProfitOnFill struct {
-	TimeInForce string `json:"timeInForce"`
-	Price       string `json:"price"`
-}
-
-//MarketBuyOrder builds struct needed for marshaling data into a []byte
-func (o Orders) MarketBuyOrder(bid float64, ask float64, instrument string, units int) Orders {
-	//tp/sl ratio is 3 to 1
-	stopLossPrice := fmt.Sprintf("%.6f", bid-(ask*.005))
-	stopLossOnFill := StopLossOnFill{TimeInForce: "GTC", Price: stopLossPrice}
-
-	takeProfitPrice := fmt.Sprintf("%.6f", ask+(ask*.015))
-	takeProfitOnFill := TakeProfitOnFill{TimeInForce: "GTC", Price: takeProfitPrice}
-
-	o.OrderData = Order{
-		StopLossOnFill:   stopLossOnFill,
-		TakeProfitOnFill: takeProfitOnFill,
-		TimeInForce:      "FOK",
-		Instrument:       instrument,
-		Type:             "MARKET",
-		PositionFill:     "DEFAULT"}
-
-	return o
-}
-
-//MarketSellOrder builds struct needed for marshaling data into a []byte
-func (o Orders) MarketSellOrder(bid float64, ask float64, instrument string, units int) Orders {
-	//tp/sl ratio is 3 to 1
-	stopLossPrice := fmt.Sprintf("%.6f", ask+(bid*.005))
-	stopLossOnFill := StopLossOnFill{TimeInForce: "GTC", Price: stopLossPrice}
-
-	takeProfitPrice := fmt.Sprintf("%.6f", bid-(ask*.015))
-	takeProfitOnFill := TakeProfitOnFill{TimeInForce: "GTC", Price: takeProfitPrice}
-
-	o.OrderData = Order{
-		StopLossOnFill:   stopLossOnFill,
-		TakeProfitOnFill: takeProfitOnFill,
-		TimeInForce:      "FOK",
-		Instrument:       instrument,
-		Type:             "MARKET",
-		PositionFill:     "DEFAULT"}
-
-	return o
-}
-
 //MarshalOrders marshals order data into []byte for making API requests
-func (o Orders) MarshalOrders(orders Orders) []byte {
+func MarshalOrders(orders Orders) []byte {
 
 	ordersByte, err := json.Marshal(orders)
 
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 
 	return ordersByte
