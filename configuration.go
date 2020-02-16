f87a5f1da29ddc5b62720e93cbae624a59e378ec
nepdave <nepdave1@gmail.com>
2020-02-15 20:52:13 -0800

    new style

--- configuration.go
+++ configuration.go
@@ -1,26 +1,14 @@
 package oanda
 
 import (
-	"log"
 	"net/http"
 	"os"
 )
 
 var (
-	oandaURL       string
-	streamoandaURL string
-	bearer         string
-	accountID      string
-	logger         *log.Logger
-	client         = &http.Client{}
-)
-
-//OandaInit populates the global variables using using the evironment variables
-func OandaInit(oandaLogger *log.Logger) {
-	logger = oandaLogger
+	client   = &http.Client{}
 	oandaURL = os.Getenv("OANDA_URL")
-	streamoandaURL = os.Getenv("STREAM_OANDA_URL")
-	bearer = "Bearer " + os.Getenv("OANDA_TOKEN")
+	//streamoandaURL = os.Getenv("STREAM_OANDA_URL")
+	bearer    = "Bearer " + os.Getenv("OANDA_TOKEN")
 	accountID = os.Getenv("OANDA_ACCOUNT_ID")
-
-}
+)
---
+++ pricing.go
@@ -0,0 +1,124 @@
+package oanda
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type Pricing struct {
+	Prices []Prices `json:"prices"`
+}
+
+type Prices struct {
+	Asks                       []Asks                     `json:"asks"`
+	Bids                       []Bids                     `json:"bids"`
+	CloseoutAsk                string                     `json:"closeoutAsk"`
+	CloseoutBid                string                     `json:"closeoutBid"`
+	Instrument                 string                     `json:"instrument"`
+	QuoteHomeConversionFactors QuoteHomeConversionFactors `json:"quoteHomeConversionFactors"`
+	Status                     string                     `json:"status"`
+	Time                       time.Time                  `json:"time"`
+	UnitsAvailable             UnitsAvailable             `json:"unitsAvailable"`
+}
+
+type Asks struct {
+	Liquidity int    `json:"liquidity"`
+	Price     string `json:"price"`
+}
+
+type Bids struct {
+	Liquidity int    `json:"liquidity"`
+	Price     string `json:"price"`
+}
+
+type QuoteHomeConversionFactors struct {
+	NegativeUnits string `json:"negativeUnits"`
+	PositiveUnits string `json:"positiveUnits"`
+}
+
+type Default struct {
+	Long  string `json:"long"`
+	Short string `json:"short"`
+}
+
+type OpenOnly struct {
+	Long  string `json:"long"`
+	Short string `json:"short"`
+}
+
+type ReduceFirst struct {
+	Long  string `json:"long"`
+	Short string `json:"short"`
+}
+
+type ReduceOnly struct {
+	Long  string `json:"long"`
+	Short string `json:"short"`
+}
+
+type UnitsAvailable struct {
+	Default     Default     `json:"default"`
+	OpenOnly    OpenOnly    `json:"openOnly"`
+	ReduceFirst ReduceFirst `json:"reduceFirst"`
+	ReduceOnly  ReduceOnly  `json:"reduceOnly"`
+}
+
+func NewPricing(instrument string) (*Pricing, error) {
+	pricing := &Pricing{}
+
+	pricesByte, err := pricing.GetPricing(instrument)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = pricing.UnmarshalPricing(pricesByte)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return pricing, nil
+
+}
+
+//GetPricing returns latest pricing data
+func (p *Pricing) GetPricing(instrument string) ([]byte, error) {
+	req, err := http.NewRequest("GET", oandaURL+"/accounts/"+accountID+"/pricing?"+instrument, nil)
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", bearer)
+	req.Header.Add("Connection", "Keep-Alive")
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	pricing, _ := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return pricing, nil
+}
+
+func (p *Pricing) UnmarshalPricing(pricing []byte) error {
+	err := json.Unmarshal(pricing, &p)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
