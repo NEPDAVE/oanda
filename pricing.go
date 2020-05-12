c3d84690e259f137dc316aeeccb4bf886bc81672
nepdave <nepdave1@gmail.com>
2020-05-12 05:56:09 -0700

    no more inline struct

--- pricing.go
+++ pricing.go
@@ -8,46 +8,56 @@ import (
 )
 
 type PricingPayload struct {
-	Prices []struct {
-		Asks []struct {
-			Liquidity int    `json:"liquidity"`
-			Price     string `json:"price"`
-		} `json:"asks"`
-		Bids []struct {
-			Liquidity int    `json:"liquidity"`
-			Price     string `json:"price"`
-		} `json:"bids"`
-		CloseoutAsk                string `json:"closeoutAsk"`
-		CloseoutBid                string `json:"closeoutBid"`
-		Instrument                 string `json:"instrument"`
-		QuoteHomeConversionFactors struct {
-			NegativeUnits string `json:"negativeUnits"`
-			PositiveUnits string `json:"positiveUnits"`
-		} `json:"quoteHomeConversionFactors"`
-		Status         string    `json:"status"`
-		Time           time.Time `json:"time"`
-		UnitsAvailable struct {
-			Default struct {
-				Long  string `json:"long"`
-				Short string `json:"short"`
-			} `json:"default"`
-			OpenOnly struct {
-				Long  string `json:"long"`
-				Short string `json:"short"`
-			} `json:"openOnly"`
-			ReduceFirst struct {
-				Long  string `json:"long"`
-				Short string `json:"short"`
-			} `json:"reduceFirst"`
-			ReduceOnly struct {
-				Long  string `json:"long"`
-				Short string `json:"short"`
-			} `json:"reduceOnly"`
-		} `json:"unitsAvailable"`
-	} `json:"prices"`
+	Prices []Prices `json:"prices"`
+}
+type Asks struct {
+	Liquidity int    `json:"liquidity"`
+	Price     string `json:"price"`
+}
+type Bids struct {
+	Liquidity int    `json:"liquidity"`
+	Price     string `json:"price"`
+}
+type QuoteHomeConversionFactors struct {
+	NegativeUnits string `json:"negativeUnits"`
+	PositiveUnits string `json:"positiveUnits"`
+}
+type Default struct {
+	Long  string `json:"long"`
+	Short string `json:"short"`
+}
+type OpenOnly struct {
+	Long  string `json:"long"`
+	Short string `json:"short"`
+}
+type ReduceFirst struct {
+	Long  string `json:"long"`
+	Short string `json:"short"`
+}
+type ReduceOnly struct {
+	Long  string `json:"long"`
+	Short string `json:"short"`
+}
+type UnitsAvailable struct {
+	Default     Default     `json:"default"`
+	OpenOnly    OpenOnly    `json:"openOnly"`
+	ReduceFirst ReduceFirst `json:"reduceFirst"`
+	ReduceOnly  ReduceOnly  `json:"reduceOnly"`
+}
+type Prices struct {
+	Asks                       []Asks                     `json:"asks"`
+	Bids                       []Bids                     `json:"bids"`
+	CloseoutAsk                string                     `json:"closeoutAsk"`
+	CloseoutBid                string                     `json:"closeoutBid"`
+	Instrument                 string                     `json:"instrument"`
+	QuoteHomeConversionFactors QuoteHomeConversionFactors `json:"quoteHomeConversionFactors"`
+	Status                     string                     `json:"status"`
+	Time                       time.Time                  `json:"time"`
+	UnitsAvailable             UnitsAvailable             `json:"unitsAvailable"`
 }
 
-//GetPricing returns latest pricing data
+//GetPricing returns latest pricing data - pricing data is returned for each
+//instrument passed in array
 func GetPricing(instruments []string) (*PricingPayload, error) {
 	instrumentsString := strings.Join(instruments, ",")
 	queryValues := url.Values{}
